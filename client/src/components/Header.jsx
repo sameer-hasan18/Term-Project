3ea43e501d7ee6ccc229225f7244ba397a274115
sameer-hasan18 <smsamhas@gmail.com>
2023-10-10 12:59:12 -0700

    first commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+    return (
+        <header className='bg-slate-200 shadow-md'>
+            <div className='flex justify-between mx-auto p-3'>
+                <Link to='/'>
+                    <h1 className='font-bold text-sm sm:text-xl flex-wrap'>
+                        <span className="text-slate-500">Hospital</span>
+                        <span className="text-slate-800">Management</span>
+                        <span className="text-slate-500">System</span>
+                    </h1>
+                </Link>
+
+                <ul className='flex gap-4 font-bold'>
+                    <Link to='/'>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                    </Link>
+
+                    <Link to='/about'>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+                    </Link>
+
+                    <Link to='/sign-in'>
+                        <li className='text-slate-700 hover:underline'>Sign in</li>
+                    </Link>
+                </ul>
+            </div>
+        </header>
+    )
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/Header.jsx",
+    "./src/index.js",
+    "./src/App.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
