7b7dc9eb17659f19ac9cff85388e15025016b12f
sameer-hasan18 <smsamhas@gmail.com>
2023-10-27 13:39:16 -0700

    Designed Home and About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Our Class Project
+      </h1>
+      <p className="mb-4 text-slate-700"></p>
+      <p className="mb-4 text-slate-700">
+        Our mission is to develop a website for a Hospital Management System.
+        The website to going to create a platform for both patients and doctors
+        to communicate and manage their interactions.{" "}
+      </p>
+      <p className="mb-4 text-slate-700">
+        Interactions such as adding appointments from the side of the patient,
+        doctors being able to see the patient’s medical information and ongoing
+        reports on the patient. The system will have access to a database to
+        store all the information on the doctors and the patients. The features
+        of this website are partially decided, depending on the feasibility of
+        the project we will keep adding features to enhance the user experience.
+      </p>
+    	<br></br>
+      <h1 className="text-xl font-bold mb-4 text-slate-800">
+        Project Members:
+      </h1>
+      <ul class="list-disc pl-7">
+        <li>  Sameer Hasan</li>
+        <li>  Ray</li>
+        <li>  Yeffry</li>
+        <li>  Thomas</li>
+        <li>  Forest</li>
+      </ul>
+    </div>
+  );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-    return (
-        <div className='text-red-500'>Hello, its Home</div>
+    return (  
+    <div>
+      {/* top */}
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+          Find and connect to our <span className='text-slate-500'>doctors</span>
+          <br />
+          with ease
+        </h1>
+        <div className='text-gray-400 text-xs sm:text-sm'>
+          Our hospital management software comes with multiple useful features.
+          <br />
+          Look through our various doctors and make appointment as you need.
+        </div>
+        <Link
+          to={'/sign-in'}
+          className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
+        >
+          Let's get started...
+        </Link>
+      </div>
+ </div>
     )
 }
+
