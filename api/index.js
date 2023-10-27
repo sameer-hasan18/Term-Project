bd256dc6e4c9505961de8267a6d63042e1ac88d0
sameer-hasan18 <smsamhas@gmail.com>
2023-10-27 09:02:45 -0700

    Added sign up api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 mongoose
     .connect("mongodb+srv://hiveatwork:hiveatwork@hospital-management.4t5xuja.mongodb.net/CMPT276?retryWrites=true&w=majority")
@@ -13,8 +14,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
