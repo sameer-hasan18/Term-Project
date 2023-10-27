9b6d469486998eb70c0f52b0cb146a417d1008f7
sameer-hasan18 <smsamhas@gmail.com>
2023-10-27 08:20:16 -0700

    Added Database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+mongoose
+    .connect("mongodb+srv://hiveatwork:hiveatwork@hospital-management.4t5xuja.mongodb.net/CMPT276?retryWrites=true&w=majority")
+    .then(() => {
+        console.log('Connected to MongoDB!');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      
 const app = express();
 
 app.listen(3000, () => {
